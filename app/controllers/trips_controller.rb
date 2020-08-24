60deaf74b5dd79247b8d818254c56a2146ea329a
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-08-24 16:28:10 +0200

    added models controllers and views

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,41 @@
+class TripsController < ApplicationController
+
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(strong_params)
+    @trip.user = current_user
+    if @trip.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    @user = @trip.user
+    @trip.destroy
+     redirect_to user_path
+  end
+
+  def update
+  end
+
+  private
+
+    def strong_params
+      params.require(:trip).permit(:date, :time, :longitude, :latitude, :wave_length, :wave_height, :air_temperature, :sea_temperature, :wind_speed, :wind_direction)
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_122905) do
+ActiveRecord::Schema.define(version: 2020_08_24_135500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.integer "latitude"
+    t.integer "longitude"
+    t.string "description"
+    t.float "wave_height"
+    t.float "wave_length"
+    t.integer "wind_speed"
+    t.integer "wind_direction"
+    t.integer "air_temperature"
+    t.integer "sea_temperature"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +41,11 @@ ActiveRecord::Schema.define(version: 2020_08_24_122905) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "trips", "users"
 end
