e195a1487a72659c66fe59963c38f980fc58b38d
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-08-25 17:59:16 +0200

    added data information to the show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_142036) do
+ActiveRecord::Schema.define(version: 2020_08_24_135500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 2020_08_25_142036) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "wind_gusts"
-    t.string "trip_type"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
