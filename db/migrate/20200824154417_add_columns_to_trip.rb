d28f2f70b2b9ef650916f6f3fec39ffe7771bcfd
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-08-25 16:12:00 +0200

    fixed schema to rollback a redundant migration and renamed trip_type as type is a reserved term

--- db/migrate/20200824154417_add_columns_to_trip.rb
+++ db/migrate/20200824154417_add_columns_to_trip.rb
@@ -1,6 +1,6 @@
 class AddColumnsToTrip < ActiveRecord::Migration[6.0]
   def change
     add_column :trips, :wind_gusts, :integer
-    add_column :trips, :type, :string
+    add_column :trips, :trip_type, :string
   end
 end
--- db/migrate/20200824163040_add_wind_gusts_to_trips.rb
+++
@@ -1,5 +0,0 @@
-class AddWindGustsToTrips < ActiveRecord::Migration[6.0]
-  def change
-    add_column :trips, :wind_gusts, :integer
-  end
-end
