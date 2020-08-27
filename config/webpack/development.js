a027135e014d0e6c9e13f7ae805045fbf55c8f52
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-08-27 13:37:49 +0200

    all bugs on trips new page now removed

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,10 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-const Dotenv = require('dotenv-webpack');
-module.exports = {
-  plugins: [
-    new Dotenv()
-  ]
-};
